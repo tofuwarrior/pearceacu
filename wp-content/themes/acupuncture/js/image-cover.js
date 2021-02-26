c4cc8d36c15115b35ff165bc026ad0cbdcea76e4
Paul Burdon <github@clearintent.co.uk>
2021-02-26 21:07:01 +0000

    Add files via upload

---
+++ wp-content/themes/acupuncture/js/image-cover.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function($){
+								
+	
+	
+	$('.img-cover').hover(function() {
+		$(this).find('.icons').stop().fadeTo(200, 1);
+		$(this).find('i').stop().fadeTo(200, 1);
+	}, function() {
+		$(this).find('.icons').stop().fadeTo(150, 0);
+		$(this).find('i').stop().delay(50).fadeTo(200, 0);
+	});
+	
+	
+
+});
---
+++ wp-content/themes/acupuncture/js/jquery.form.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function(){
+
+	$('#contactform').submit(function(){
+
+		var action = $(this).attr('action');
+
+		$("#message").slideUp(700,function() {
+		$('#message').hide();
+
+ 		$('#submit')
+			.attr('disabled','disabled');
+
+		$.post(action, {
+			name: $('#name').val(),
+			email: $('#email').val(),
+			phone: $('#phone').val(),
+			comments: $('#comments').val()
+		},
+			function(data){
+				document.getElementById('message').innerHTML = data;
+				$('#message').slideDown('slow');
+				$('#submit').removeAttr('disabled');
+				if(data.match('success') != null) $('#contactform').fadeOut('slow');
+
+			}
+		);
+
+		});
+
+		return false;
+
+	});
+
+});
